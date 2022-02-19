396e9770b12fb539b1de87149ef274232a3e3c8c
Gourie <gouriejagtap@gmail.com>
2022-02-20 00:22:33 +0530

    commit3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let togglebtn=document.getElementsByClassName('toggle-btn')[0]
 let nav=document.getElementsByClassName('navbr')[0]
 
-togglebtn.addeventListener('click',function(){
+togglebtn.addEventListener('click',function(){
     nav.classList.toggle('active')
 })
